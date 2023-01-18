58b2de91d188109675d7825e5e2cdc45eea079af
Dan Stronge <dnlstronge@gmail.com>
2023-01-18 16:21:57 +0000

    [x]- used props to output dynamic values in styled component

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,43 +1,40 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+
 
 
 const FormControl = styled.div`
-  
+ 
   margin: 0.5rem 0;
 
 
-  &label {
+  & label {
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${props => props.invalid ? 'red' : 'black'}
   }
 
-  &input {
+  & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${props => props.invalid ? 'red': '#ccc'};
+    background: ${props => props.invalid ? 'rgb(238, 200, 196)' : 'transparent'}
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
   }
 
-  &input:focus {
+  & input:focus {
     outline: none;
     background: #fad0ec;
     border-color: #8b005d;
   }
 
-  &.invalid input {
-    border-color: red;
-    background-color: rgb(238, 200, 196);
-  }
+ 
 
-  &.invalid label {
-    color: red;
-  }
+  
 `; 
 
 const CourseInput = props => {
@@ -64,7 +61,7 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={`form-control ${!isValid && 'invalid'}`} >
+      <FormControl invalid={!isValid} >
         <label>Course Goal</label>
         <input 
           type="text" 
